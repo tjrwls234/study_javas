b5c65c2f1ab19506240a4284d458155c8acb635c
tjrwls234 <tjrwls234@gmail.com>
2022-10-31 14:55:13 +0900

    커밋

---
+++ README.md
@@ -0,0 +1,6 @@
+# 작성 소스
+마크다운 언어를 사용합니다.  
+사용이 편리합니다.  
+[google link](https://www.google.com)
+
+- [MinusAB.java](https://github.com/tjrwls234/work/blob/master/Study_JAVAS/src/MinusAB.java) : 두 변수를 뺌.
---
+++ src/MinusAB.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MinusAB{
+    //두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+    
+    public static void main(String[] args) {
+        //입력
+        //첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+        Scanner scanner  = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        
+        //출력
+        System.out.println(A-B);
+        scanner.close();
+    }
+    
+    
+}
